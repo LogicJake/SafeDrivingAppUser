094dd831e50c729bf494563cb1d9471c761e6a40
NepoleonAsus <835410808@qq.com>
2018-01-27 20:15:22 +0800

    个人信息初步框架

--- app/src/main/java/com/nuaa/safedriving/MainActivity.java
+++ app/src/main/java/com/nuaa/safedriving/MainActivity.java
@@ -1,8 +1,12 @@
 package com.nuaa.safedriving;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.ListViewAutoScrollHelper;
 import android.support.v7.widget.ListPopupWindow;
 import android.view.View;
@@ -23,14 +27,67 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Home home;
+    private UserInfo userInfo;
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
-    private ListView lv;
-    private SimpleAdapter mSchedule;
-    private List<HashMap<String, Object>> mListData = new ArrayList<HashMap<String, Object>>();
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    initHome();
+                    return true;
+                case R.id.navigation_info:
+                    initUserInfo();
+                    return true;
+            }
+            return false;
+        }
+
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        if (savedInstanceState != null) { // “内存重启”时调用
+            initHome();
+        }else{      //正常启动时调用
+            initHome();
+        }
+    }
 
+    private void initHome() {
+        //开启事务，fragment的控制是由事务来实现的
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        if(home == null){
+            home = new Home();
+            transaction.add(R.id.content,home,"home");
+        }
+        hideFragment(transaction);
+        transaction.show(home);
+        transaction.commit();
+    }
+
+    private void initUserInfo(){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        if(userInfo == null){
+            userInfo = new UserInfo();
+            transaction.add(R.id.content,userInfo,"userinfo");
+        }
+        hideFragment(transaction);
+        transaction.show(userInfo);
+        transaction.commit();
+    }
+
+    public void hideFragment(FragmentTransaction transaction){
+        if(home != null){
+            transaction.hide(home);
+        }
+        if(userInfo != null){
+            transaction.hide(userInfo);
+        }
     }
 }
--- app/src/main/java/com/nuaa/safedriving/UserInfo.java
+++ app/src/main/java/com/nuaa/safedriving/UserInfo.java
@@ -74,16 +74,6 @@ public class UserInfo extends Fragment {
         }
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
 
     @Override
     public void onDetach() {
